a3e873b3b9fa0b527547b281ca8bb82d1a4da14b
le <75990171+Leung190299@users.noreply.github.com>
2026-04-21 11:27:51 +0700

    Update NFC message for passport reading instructions

--- ios/NfcPassportReader.swift
+++ ios/NfcPassportReader.swift
@@ -63,7 +63,7 @@ class NfcPassportReader: NSObject {
       let customMessageHandler: (NFCViewDisplayMessage) -> String? = { displayMessage in
         switch displayMessage {
         case .requestPresentPassport:
-          return "Giữ iPhone gần Thẻ CCCD gắn chip / Hộ chiếu."
+          return "Giữ iPhone gần Thẻ CCCD/CC gắn chip."
         case .successfulRead:
           return "Đọc thẻ thành công."
         case .readingDataGroupProgress(let dataGroup, let progress):
